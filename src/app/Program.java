ed26cee5f43de5032f82d96bac50098a3c83a23d
Arthur Kramberger <arthur1curi@gmail.com>
2024-08-20 18:09:53 -0300

    Department entity class

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,14 +1,12 @@
 package app;
 
-import java.sql.Connection;
-
-import db.DB;
+import models.Department;
 
 public class Program {
     public static void main(String[] args) {
-        
-    Connection conn = DB.getConnection();
-    DB.closeConnection();
     
+    Department obj = new Department(1,"Books");
+    System.out.println(obj);
+
     }
 }
