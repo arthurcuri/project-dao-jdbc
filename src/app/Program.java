f6b53653e3979b5aa2e2a80b69ce993b982126cb
Arthur Kramberger <arthur1curi@gmail.com>
2024-08-20 18:15:23 -0300

    Seller entity class

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,12 +1,17 @@
 package app;
 
+import java.util.Date;
+
 import models.Department;
+import models.Seller;
 
 public class Program {
     public static void main(String[] args) {
     
     Department obj = new Department(1,"Books");
+    Seller sel = new Seller(1,"Caio","Email",new Date(),3000.0,obj);
     System.out.println(obj);
+    System.out.println(sel);
 
     }
 }
