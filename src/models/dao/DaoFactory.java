b14a158f8ebe21b4ff8cbb5d3b3dc46d368bee8e
Arthur Kramberger <arthur1curi@gmail.com>
2024-08-20 21:31:03 -0300

    DepartmentDao implementation and test

--- src/models/dao/DaoFactory.java
+++ src/models/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package models.dao;
 
 import db.DB;
+import models.dao.impl.DepartmentDaoJDBC;
 import models.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
@@ -8,4 +9,8 @@ public class DaoFactory {
     public static SellerDao createSellerDao(){
         return new SellerDaoJDBC(DB.getConnection());
     }
+
+    public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
 }
